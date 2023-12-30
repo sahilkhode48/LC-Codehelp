4a7fe05f7be77dde0e277495220a90125252e1f9
sahilkhode48 <109568703+sahilkhode48@users.noreply.github.com>
2023-12-31 01:41:23 +0530

    Create 1.valid-anagram.cpp

---
+++ 5. Char-Array & Strings/1.valid-anagram.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        /*sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t)
+        return true;
+
+        return false;
+        */
+
+        int freq[256]={0};
+        for(int i=0;i<s.size();i++){
+            freq[s[i]]++;
+        }
+
+        for(int i=0;i<t.size();i++){
+            freq[t[i]]--;
+        }
+
+        for(int i=0;i<256;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
