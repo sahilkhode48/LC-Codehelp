3a13d5263d2843e38209e79d25e29907a1113d48
sahilkhode48 <109568703+sahilkhode48@users.noreply.github.com>
2023-12-31 01:42:06 +0530

    Create 2.Reverse Only Letters.cpp

---
+++ 5. Char-Array & Strings/2.Reverse Only Letters.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/reverse-only-letters/
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int l=0, h=s.size()-1;
+        while(l<h){
+            if(isalpha(s[l]) && isalpha(s[h])){
+                swap(s[l] , s[h]);
+                l++; h--;
+            }
+            else if(isalpha(s[l]) && !isalpha(s[h])){
+                h--;
+            }
+            else l++;
+            
+        }
+
+        return s;
+    }
+};
