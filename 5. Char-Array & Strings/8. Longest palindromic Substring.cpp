35dae614b361d4726cc533702c9b175982fa33a8
sahilkhode48 <109568703+sahilkhode48@users.noreply.github.com>
2023-12-31 10:38:23 +0530

    Create 8. Longest palindromic Substring.cpp

---
+++ 5. Char-Array & Strings/8. Longest palindromic Substring.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution {
+public:
+    bool isPalindrome(string s, int i, int j){
+        //int start = i, end= j;
+        if (j-i==1) return true;
+        while(i<j){
+            if(s[i]!=s[j] ){
+                return false;
+            }
+            i++; j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        string ans;
+        //find substring and check if palindromic and check max lengt one
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                // passing strings and endpoints
+                if(isPalindrome(s , i , j)){
+                    string t= s.substr(i,j-1+1);
+                    ans = t.size()>ans.size() ? t:ans;
+
+                }
+
+            }
+        }
+
+        return ans;
+
+
+    }
+};
