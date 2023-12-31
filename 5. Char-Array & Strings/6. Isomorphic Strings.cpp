a209245a94f12c0ffefd52f79d0a7ee782ec4fea
sahilkhode48 <109568703+sahilkhode48@users.noreply.github.com>
2023-12-31 10:16:46 +0530

    Create 6. Isomorphic Strings.cpp

---
+++ 5. Char-Array & Strings/6. Isomorphic Strings.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/isomorphic-strings/description/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int mapp[256] = {0};
+        bool ismapped[256]= {0};
+        int n=s.size()-1;
+        //first we map and also set is mapped, if mapped already we dont change the mapp
+        for(int i=0;i<=n;i++){
+            if(mapp[s[i]]==0 && (ismapped[t[i]]==0)){
+                mapp[s[i]]=t[i];
+                ismapped[t[i]]=true;
+
+            }
+            
+        }
+        //now we iterate and check if all are mapped;
+
+        for(int i=0;i<=n;i++){
+            if(char(mapp[s[i]])!= t[i]) return false;
+        }
+        return true;
+
+        
+    }
+};
